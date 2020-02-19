39dd7b94369ce1deac05f4b447953e19cc378e5a
Alouiscious Media <info@reneesoul.com>
2020-02-18 19:53:51 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time24Str) {
+  const greetTime = parseInt(time24Str, 10);
+  if (greetTime < 12) return "Good Morning"
+  if (greetTime > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msgTime) {
+  msgTime = document.getElementById("greeting").innerText;
+}
